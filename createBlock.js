be6dae6bd560735335713978153ce2e7ca72ad6a
i.vtorov <i.vtorov@npo-echelon.ru>
2017-08-16 19:06:25 +0300

    Update #1

--- createBlock.js
+++ createBlock.js
@@ -1,7 +1,6 @@
 'use strict';
 
-// Генератор файлов блока
-
+// Генератор файлов блока (позаимстован отсюда: https://github.com/nicothin/NTH-start-project)
 // Использование: node createBlock.js [имя блока] [доп. расширения через пробел]
 
 const fs = require('fs');
@@ -10,35 +9,31 @@ const projectConfig = require('./projectConfig.json');
 const dirs = projectConfig.dirs;
 const mkdirp = require('mkdirp');
 
-const blockName = process.argv[2];          // получим имя блока
-const defaultExtensions = ['scss', 'html', 'img']; // расширения по умолчанию
+const blockName = process.argv[2];                                                // получим имя блока
+const defaultExtensions = ['scss', 'html'];                                       // расширения по умолчанию
 const extensions = uniqueArray(defaultExtensions.concat(process.argv.slice(3)));  // добавим введенные при вызове расширения (если есть)
 
 // Если есть имя блока
 if (blockName) {
-  const dirPath = `${dirs.srcPath + dirs.blocksDirName}/${blockName}/`; // полный путь к создаваемой папке блока
-  mkdirp(dirPath, (err) => {                                           // создаем
-    // Если какая-то ошибка — покажем
+  const dirPath = `${dirs.srcPath + dirs.blocksDirName}/${blockName}/`;           // полный путь к создаваемой папке блока
+  mkdirp(dirPath, (err) => {                                                      // создаем папку
     if (err) {
-      console.error(`[NTH] Отмена операции: ${err}`);
+      console.error(`Отмена операции: ${err}`);                                   // если есть ошибки — выводим в консоль
     }
 
-    // Нет ошибки, поехали!
     else {
-      console.log(`[NTH] Создание папки ${dirPath} (если отсутствует)`);
-
-      // Обходим массив расширений и создаем файлы, если они еще не созданы
-      extensions.forEach((extention) => {
-        const filePath = `${dirPath + blockName}.${extention}`; // полный путь к создаваемому файлу
-        let fileContent = '';                                 // будущий контент файла
-        let fileCreateMsg = '';                               // будущее сообщение в консоли при создании файла
+      console.log(`Создание папки ${dirPath} (если отсутствует)`);                // нет ошибки, поехали!
+      extensions.forEach((extention) => {                                         // Обходим массив расширений и создаем файлы, если они еще не созданы
+        const filePath = `${dirPath + blockName}.${extention}`;                   // полный путь к создаваемому файлу
+        let fileContent = '';                                                     // будущий контент файла
+        let fileCreateMsg = '';                                                   // будущее сообщение в консоли при создании файла
 
         // Если это SCSS
         if (extention === 'scss') {
-          fileContent = `// В этом файле должны быть стили для БЭМ-блока ${blockName}, его элементов, \n// модификаторов, псевдоселекторов, псевдоэлементов, @media-условий...\n// Очередность: http://nicothin.github.io/idiomatic-pre-CSS/#priority\n\n.${blockName} {\n\n  $block-name:                &; // #{$block-name}__element {}\n\n}\n`;
+          fileContent = `// В этом файле должны быть стили для БЭМ-блока ${blockName}, его элементов, \n// модификаторов, псевдоселекторов, псевдоэлементов, @media-условий...\n`;
           // fileCreateMsg = '';
 
-          // Добавим созданный файл
+          // Добавим созданный файл в массив блоков в projectConfig.json
           let hasThisBlock = false;
           for (const block in projectConfig.blocks) {
             if (block === blockName) {
@@ -50,13 +45,13 @@ if (blockName) {
             projectConfig.blocks[blockName] = [];
             const newPackageJson = JSON.stringify(projectConfig, '', 2);
             fs.writeFileSync('./projectConfig.json', newPackageJson);
-            fileCreateMsg = '[NTH] Подключение блока добавлено в projectConfig.json';
+            fileCreateMsg = 'Подключение блока добавлено в projectConfig.json';
           }
         }
 
         // Если это HTML
         else if (extention === 'html') {
-          fileContent = `<!--DEV\n\nДля использования этого файла как шаблона:\n\n@ @include('blocks/${blockName}/${blockName}.html')\n\n(Нужно убрать пробел между символами @)\nПодробнее: https://www.npmjs.com/package/gulp-file-include\n\n-->\n\n<div class="${blockName}">content</div>\n`;
+          fileContent = `<!--DEV\n\nДля использования этого файла как шаблона:\n\n//= _include/${blockName}/${blockName}.html\n\n-->\n\n<div class="${blockName}">content</div>\n`;
           // fileCreateMsg = '';
         }
 
@@ -71,10 +66,10 @@ if (blockName) {
           if (fileExist(imgFolder) === false) {
             mkdirp(imgFolder, (err) => {
               if (err) console.error(err);
-              else console.log(`[NTH] Создание папки: ${imgFolder} (если отсутствует)`);
+              else console.log(`Создание папки: ${imgFolder} (если отсутствует)`);
             });
           } else {
-            console.log(`[NTH] Папка ${imgFolder} НЕ создана (уже существует) `);
+            console.log(`Папка ${imgFolder} НЕ создана (уже существует) `);
           }
         }
 
@@ -82,21 +77,21 @@ if (blockName) {
         if (fileExist(filePath) === false && extention !== 'img') {
           fs.writeFile(filePath, fileContent, (err) => {
             if (err) {
-              return console.log(`[NTH] Файл НЕ создан: ${err}`);
+              return console.log(`Файл НЕ создан: ${err}`);
             }
-            console.log(`[NTH] Файл создан: ${filePath}`);
+            console.log(`Файл создан: ${filePath}`);
             if (fileCreateMsg) {
               console.warn(fileCreateMsg);
             }
           });
         } else if (extention !== 'img') {
-          console.log(`[NTH] Файл НЕ создан: ${filePath} (уже существует)`);
+          console.log(`Файл НЕ создан: ${filePath} (уже существует)`);
         }
       });
     }
   });
 } else {
-  console.log('[NTH] Отмена операции: не указан блок');
+  console.log('Отмена операции: не указан блок');
 }
 
 // Оставить в массиве только уникальные значения (убрать повторы)
--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,49 @@
 "use strict";
 
-var gulp = require("gulp");
-var sass = require("gulp-sass");
-var plumber = require("gulp-plumber");
-var postcss = require("gulp-postcss");
-var rigger = require("gulp-rigger");
-var autoprefixer = require("autoprefixer");
-var mqpacker = require("css-mqpacker");
-var minify = require("gulp-csso");
-var uglify = require("gulp-uglify");
-var imagemin = require("gulp-imagemin");
-var rename = require("gulp-rename");
-var svgmin = require("gulp-svgmin");
-var svgstore = require("gulp-svgstore");
-var run = require("run-sequence");
-var del = require("del");
-var server = require("browser-sync");
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
+var postcss = require('gulp-postcss');
+var rigger = require('gulp-rigger');
+var autoprefixer = require('autoprefixer');
+var mqpacker = require('css-mqpacker');
+var minify = require('gulp-csso');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');
+var rename = require('gulp-rename');
+var svgmin = require('gulp-svgmin');
+var svgstore = require('gulp-svgstore');
+var run = require('run-sequence');
+var del = require('del');
+var server = require('browser-sync');
 var reload = server.reload;
 
 var path = {
   public: {
-    css: "build/css",
-    js: "build/js",
-    img: "build/img",
-    fonts: "build/fonts"
+    css: 'build/css',
+    js: 'build/js',
+    img: 'build/img',
+    fonts: 'build/fonts'
   },
   src: {
-    html: "src/*.html",
-    style: "src/sсss/style.scss",
-    js: "src/js/*.js",
-    images: "src/img/**/*.*",
-    fonts: "src/fonts/**/*.*"
+    html: 'src/*.html',
+    style: 'src/sсss/style.scss',
+    js: 'src/js/*.js',
+    images: 'src/img/**/*.*',
+    fonts: 'src/fonts/**/*.*'
   },
   watch: {
-    html: "src/**/*.html",
-    style: "src/scss/**/*.scss",
+    html: 'src/**/*.html',
+    style: 'src/scss/**/*.scss',
     js: "src/js/**/*.js",
-    images: "src/img/**/*.*",
-    fonts: "src/fonts/**/*.*"
+    images: 'src/img/**/*.*',
+    fonts: 'src/fonts/**/*.*'
   },
-  production: "build/"
+  production: 'build/'
 }
 
 // Локальный сервер + слежение за изменением файлов
-gulp.task("serve", function() {
+gulp.task('serv', function() {
   server({
     server: {
       baseDir: path.production
@@ -52,17 +52,17 @@ gulp.task("serve", function() {
     open: true,
     notify: false
   });
-  gulp.watch(path.watch.html, ["html"])
-  gulp.watch(path.watch.style, ["css"]);
+  gulp.watch(path.watch.html, ['html'])
+  gulp.watch(path.watch.style, ['css']);
 });
 
 // Очистка папки build
-gulp.task("clean", function() {
+gulp.task('clean', function() {
   return del(path.production);
 });
 
 // Cборка html
-gulp.task("html", function () {
+gulp.task('html', function () {
   gulp.src(path.src.html)
     .pipe(rigger())
     .pipe(gulp.dest(path.production))
@@ -70,8 +70,8 @@ gulp.task("html", function () {
 });
 
 // Сборка стилей
-gulp.task("css", function() {
-  gulp.src("src/scss/style.scss")
+gulp.task('css', function() {
+  gulp.src('src/scss/style.scss')
     .pipe(plumber())
     .pipe(sass())
     .pipe(postcss([
@@ -83,13 +83,13 @@ gulp.task("css", function() {
     ]))
     .pipe(gulp.dest(path.public.css))
     .pipe(minify())
-    .pipe(rename("style-min.css"))
+    .pipe(rename('style-min.css'))
     .pipe(gulp.dest(path.public.css))
     .pipe(reload({stream: true}));
 });
 
 // Оптимизация изображений
-gulp.task("images", function() {
+gulp.task('images', function() {
   return gulp.src(path.src.images)
     .pipe(imagemin([
       imagemin.optipng({optimizationLevel: 3}),
@@ -100,7 +100,7 @@ gulp.task("images", function() {
 });
 
 // Оптимизация JS
-gulp.task("js", function () {
+gulp.task('js', function () {
   gulp.src(path.src.js)
     .pipe(uglify())
     .pipe(rename({suffix: '.min'}))
@@ -109,19 +109,19 @@ gulp.task("js", function () {
 });
 
 // Копирование шрифтов в папку build
-gulp.task("copy", function() {
+gulp.task('copy', function() {
   gulp.src(path.src.fonts)
     .pipe(gulp.dest(path.public.fonts));
 });
 
 // Запуск сборки проекта
-gulp.task("build", function() {
+gulp.task('build', function() {
   run(
-    "clean",
-    "html",
-    "css",
-    "images",
-    "js",
-    "copy"
+    'clean',
+    'html',
+    'css',
+    'images',
+    'js',
+    'copy'
   );
 });
